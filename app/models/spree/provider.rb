93a8da3ebef81a20fd3c299e295d9f376b6d5ca2
Robson Fernandes <robfds@gmail.com>
2016-11-15 20:36:07 -0300

    Inplementing validations on model provider

--- app/models/spree/provider.rb
+++ app/models/spree/provider.rb
@@ -1,3 +1,18 @@
 class Spree::Provider < ActiveRecord::Base
+  # Validations
+  validates_presence_of :nome_fantasia, :razao_social, :cnpj_cpf
+  validates_length_of :estado, is: 2, allow_blank: false
+  validate :validate_cpf_cnpj
+
+  # Associations
   has_many :spree_products
+
+  private
+  def validate_cpf_cnpj
+    cpf = BRDocuments::CPF
+    cnpj = BRDocuments::CNPJ
+    errors.add(:cnpj_cpf, :cnpj_or_cpf_invalid) unless (
+      cpf.valid?(cnpj_cpf) || cnpj.valid?(cnpj_cpf)
+    )
+  end
 end
