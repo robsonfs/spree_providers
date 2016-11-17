49d67289d84e8ff16bcba410c167aa48b013bbef
Robson Fernandes <robfds@gmail.com>
2016-11-17 10:19:38 -0300

    Implementing actions show and edit for providers

--- app/controllers/spree/admin/providers_controller.rb
+++ app/controllers/spree/admin/providers_controller.rb
@@ -2,4 +2,13 @@ class Spree::Admin::ProvidersController < Spree::Admin::ResourceController
   def index
     @collection = Spree::Provider.all
   end
+
+  def edit
+    @provider = Spree::Provider.find(params[:id])
+  end
+
+  def show
+    session[:return_to] ||= request.referer
+    redirect_to action: :edit
+  end
 end
